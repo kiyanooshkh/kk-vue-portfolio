049cfd9ecc809912c4485c925c08ec0e1b58d9a0
Kiyanoosh Khosravi <Kiyanoosh.Khosravi@certegy.com.au>
2019-09-10 14:55:54 +0930

    add spinner or loading

--- src/views/Photography.vue
+++ src/views/Photography.vue
@@ -1,5 +1,6 @@
 <template>
   <v-sheet :tile="noRadius" class="overflow-hidden">
+    <Spinner v-if="grams.length == 0" />
     <div class="pt-0 pb-0 pl-2">
       <v-row>
         <v-col cols="12" md="12" lg="3" class="primary textwhite">
@@ -17,15 +18,6 @@
         <v-col cols="12" md="12" lg="9" class="pt-0 pb-0">
           <v-row>
             <v-col class="pt-0 pb-0">
-              <div class="loading" v-if="grams.length == 0">
-                <div class="spinner-locator">
-                <div class="spinner">
-                  <div class="double-bounce1"></div>
-                  <div class="double-bounce2"></div>
-                </div>
-                </div>
-              </div>
-
               <template v-if="grams.length > 0">
                 <v-row>
                   <v-col
@@ -64,10 +56,11 @@
 
 <script>
 import Navbar from "../components/Navbar";
+import Spinner from "../components/Spinner";
 import axios from "axios";
 
 export default {
-  components: { Navbar },
+  components: { Navbar, Spinner },
   data() {
     return {
       alignment: "center",
@@ -119,72 +112,8 @@ export default {
 };
 </script>
 <style scoped>
-.loading {
-  position: fixed;
-  z-index: 999;
-  overflow: hidden;
-  margin: auto;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background-color: #086788;
-}
 .slide-left {
   margin-top: 5%;
-}
-
-.spinner {
-  width: 40px;
-  height: 40px;
-  position: relative;
-  margin: 0 auto;
-}
-
-.spinner-locator {
-  margin-top: 20%;
-}
-
-.double-bounce1,
-.double-bounce2 {
-  width: 100%;
-  height: 100%;
-  border-radius: 50%;
-  background-color: #fff;
-  opacity: 0.6;
-  position: absolute;
-  top: 0;
-  left: 0;
-
-  -webkit-animation: sk-bounce 2s infinite ease-in-out;
-  animation: sk-bounce 2s infinite ease-in-out;
-}
-
-.double-bounce2 {
-  -webkit-animation-delay: -1s;
-  animation-delay: -1s;
-}
-
-@-webkit-keyframes sk-bounce {
-  0%,
-  100% {
-    -webkit-transform: scale(0);
-  }
-  50% {
-    -webkit-transform: scale(1);
-  }
-}
-
-@keyframes sk-bounce {
-  0%,
-  100% {
-    transform: scale(0);
-    -webkit-transform: scale(0);
-  }
-  50% {
-    transform: scale(1);
-    -webkit-transform: scale(1);
-  }
 }
 
 @media only screen and (max-width: 1024px) {
