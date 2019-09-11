a03c2d7bcd117270ecf72d2895ec3c521cd99dd4
Kiyanoosh Kh <khosravi.kiyanoosh@gmail.com>
2019-09-11 23:25:41 +0930

    Update Portfolio.vue

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -16,12 +16,37 @@
         </v-col>
         <v-col cols="12" md="12" lg="9" class="pt-0 pb-0">
           <v-row>
-            <v-col v-for="item in items" :key="item.img" no-gutters class="pa-0" cols="12" sm="6" md="6">
-              <v-img
-                :src="item.img"
-                :lazy-src="item.img"
-                class="center grey lighten-2"
-              />
+            <v-col
+              v-for="item in items"
+              :key="item.img"
+              no-gutters
+              class="pa-0"
+              cols="12"
+              sm="6"
+              md="6"
+            >
+              <v-hover v-slot:default="{ hover }">
+                <v-img aspect-ratio="2" :src="item.img" :lazy-src="item.img" class="center grey lighten-2">
+                  <v-expand-transition>
+                    <div
+                      v-if="hover"
+                      class="d-flex transition-fast-in-fast-out primary darken-2 v-card--reveal display-3 white--text"
+                      style="height: 100%;"
+                    >
+                      <v-row class="text-center">
+                        <v-col cols="12">
+                          <span class="title">{{item.name}}</span>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-btn :href="item.url" target="_blank" class="mx-2" fab dark large color="secondary">
+                            <v-icon dark>mdi-open-in-new</v-icon>
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+                    </div>
+                  </v-expand-transition>
+                </v-img>
+              </v-hover>
             </v-col>
           </v-row>
         </v-col>
@@ -39,19 +64,28 @@ export default {
     return {
       noRadius: true,
       items: [
-        { img: require('@/assets/portfolio/monaviphotography.jpg')},
-        { img: require('@/assets/portfolio/diamondeventshire.jpg')},
-        { img: require('@/assets/portfolio/phukettraining.jpg')},
-        { img: require('@/assets/portfolio/iisinsulation.jpg')},
-        { img: require('@/assets/portfolio/tamarahuntington.jpg')},
-        { img: require('@/assets/portfolio/escrooms.jpg')}
+        { img: require("@/assets/portfolio/monaviphotography.jpg"), url:"http://monaviphotography.com/", name:"Monavi Photography" },
+        { img: require("@/assets/portfolio/diamondeventshire.jpg"), url:"https://www.diamondeventshire.com.au", name:"Diamond Events Hire" },
+        { img: require("@/assets/portfolio/phukettraining.jpg"), url:"http://www.phukettraining.com.au/", name:"Phuket Training" },
+        { img: require("@/assets/portfolio/iisinsulation.jpg"), url:"http://www.iisinsulation.com.au/", name:"Iprimus Insulation Solution" },
+        { img: require("@/assets/portfolio/tamarahuntington.jpg"), url:"https://tamarahuntington.com.au/", name:"Tamara Huntington" },
+        { img: require("@/assets/portfolio/escrooms.jpg"), url:"http://escrooms.com.au", name:"ESC Rooms" },
       ]
     };
   }
 };
 </script>
 <style scoped>
+
 .slide-left {
   margin-top: 5%;
+}
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.9;
+  position: absolute;
+  width: 100%;
 }
 </style>
