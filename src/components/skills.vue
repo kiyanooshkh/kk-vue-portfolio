2e3188282b69f54e98e6d8754b3187fab64ea4f4
Kiyanoosh Khosravi <Kiyanoosh.Khosravi@certegy.com.au>
2019-09-12 15:58:25 +0930

    Update skills.vue

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -16,146 +16,148 @@
 
 <script>
 export default {
-  data: () => ({
-    items: [
-      {
-        title: "Programming",
-        skills: [
-          {
-            name: "C#"
-          },
-          {
-            name: ".Net"
-          },
-          {
-            name: "Vue.Js"
-          },
-          {
-            name: "Angular 2"
-          },
-          {
-            name: "Aurelia"
-          },
-          {
-            name: "Typescript"
-          },
-          {
-            name: "JavaScript"
-          },
-          {
-            name: "JQuery"
-          },
-          {
-            name: "ASP.NET"
-          },
-          {
-            name: "MVC"
-          },
-          {
-            name: "PHP"
-          },
-          {
-            name: "HTML 5"
-          },
-          {
-            name: "Sass/CSS"
-          },
-          {
-            name: "Bootstrap"
-          },
-          {
-            name: "Materialize CSS"
-          },
-          {
-            name: "Vuetify.js"
-          }
-        ]
-      },
-      {
-        title: "Databases",
-        skills: [
-          {
-            name: "Microsoft SQL"
-          },
-          {
-            name: "Oracle"
-          },
-          {
-            name: "MySQL"
-          },
-          {
-            name: "LINQ"
-          },
-          {
-            name: "SSIS Packages"
-          },
-          {
-            name: "Advanced Normalisation"
-          },
-          {
-            name: "Unified Modelling Language (UML)"
-          }
-        ]
-      },
-      {
-        title: "Enterprise",
-        skills: [
-          {
-            name: "Deployment Automation"
-          },
-          {
-            name: "Business Analysis"
-          },
-          {
-            name: "Business Processes"
-          },
-          {
-            name: "Change Management"
-          },
-          {
-            name: "Agile Methodologies"
-          },
-          {
-            name: "Requirement Analysis"
-          }
-        ]
-      },
-      {
-        title: "Graphic",
-        skills: [
-          {
-            name: "Photoshop"
-          },
-          {
-            name: "Illustrator"
-          },
-          {
-            name: "UI and UX Design"
-          }
-        ]
-      },
-      {
-        title: "Personal Skills",
-        skills: [
-          {
-            name: "Mentoring"
-          },
-          {
-            name: "Time management"
-          },
-          {
-            name: "Communication"
-          },
-          {
-            name: "Leadership"
-          },
-          {
-            name: "Presentations"
-          }
-        ]
-      }
-    ]
-  })
+  data() {
+    return {
+      items: [
+        {
+          title: "Programming",
+          skills: [
+            {
+              name: "C#"
+            },
+            {
+              name: ".Net"
+            },
+            {
+              name: "Vue.Js"
+            },
+            {
+              name: "Angular 2"
+            },
+            {
+              name: "Aurelia"
+            },
+            {
+              name: "Typescript"
+            },
+            {
+              name: "JavaScript"
+            },
+            {
+              name: "JQuery"
+            },
+            {
+              name: "ASP.NET"
+            },
+            {
+              name: "MVC"
+            },
+            {
+              name: "PHP"
+            },
+            {
+              name: "HTML 5"
+            },
+            {
+              name: "Sass/CSS"
+            },
+            {
+              name: "Bootstrap"
+            },
+            {
+              name: "Materialize CSS"
+            },
+            {
+              name: "Vuetify.js"
+            }
+          ]
+        },
+        {
+          title: "Databases",
+          skills: [
+            {
+              name: "Microsoft SQL"
+            },
+            {
+              name: "Oracle"
+            },
+            {
+              name: "MySQL"
+            },
+            {
+              name: "LINQ"
+            },
+            {
+              name: "SSIS Packages"
+            },
+            {
+              name: "Advanced Normalisation"
+            },
+            {
+              name: "Unified Modelling Language (UML)"
+            }
+          ]
+        },
+        {
+          title: "Enterprise",
+          skills: [
+            {
+              name: "Deployment Automation"
+            },
+            {
+              name: "Business Analysis"
+            },
+            {
+              name: "Business Processes"
+            },
+            {
+              name: "Change Management"
+            },
+            {
+              name: "Agile Methodologies"
+            },
+            {
+              name: "Requirement Analysis"
+            }
+          ]
+        },
+        {
+          title: "Graphic",
+          skills: [
+            {
+              name: "Photoshop"
+            },
+            {
+              name: "Illustrator"
+            },
+            {
+              name: "UI and UX Design"
+            }
+          ]
+        },
+        {
+          title: "Personal Skills",
+          skills: [
+            {
+              name: "Mentoring"
+            },
+            {
+              name: "Time management"
+            },
+            {
+              name: "Communication"
+            },
+            {
+              name: "Leadership"
+            },
+            {
+              name: "Presentations"
+            }
+          ]
+        }
+      ]
+    };
+  }
 };
 </script>
 
