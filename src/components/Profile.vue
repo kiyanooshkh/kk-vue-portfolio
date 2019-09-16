15b2eeff994af4b1c6dc3e7450a1ac6e072859c2
Kiyanoosh Khosravi <Kiyanoosh.Khosravi@certegy.com.au>
2019-09-16 10:14:00 +0930

    update home page style

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <v-row align="center" justify="center">
-      <v-img src="../assets/profile.svg" max-width="250" max-height="300"></v-img>
+      <v-img src="../assets/profile.svg" max-width="230" max-height="300"></v-img>
     </v-row>
     <br />
     <br />
-    <p class="display-1 font-weight-black white--text">KIYANOOSH KHOSRAVI</p>
+   
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,26 +3,17 @@
     <Spinner v-if="!show" />
     <div class="pt-0 pb-0 pl-2">
       <v-row>
-        <v-col cols="12" sm="6" md="6" class="primary full-height textwhite">
+        <v-col cols="12" class="primary full-height textwhite">
           <v-row>
             <v-col class="pt-0" cols="12">
               <Navbar />
             </v-col>
           </v-row>
-          <v-row class="slide-left">
-            <v-col class="text-center">
-              <Profile />
-              <div class="d-block d-sm-none">
-                <p class="headline font-weight-black white--text">WEB DEVELOPER, UI AND UX DESIGNER</p>
-              </div>
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col cols="12" sm="6" md="6" class="d-none d-sm-block">
-          <v-row class="slide-right">
-            <v-col class="text-center">
-              <p class="display-3 font-weight-black grey--text">WEB DEVELOPER,</p>
-              <p class="display-3 font-weight-black grey--text">UI AND UX DESIGNER</p>
+          <v-row class="top text-center">
+            <v-col cols="12">
+              <Profile/>
+              <div class="display-2 font-weight-bold white--text mb-5">I am Kiyanoosh Khosravi</div>
+              <div class="display-2 font-weight-light white--text mb-2">WEB DEVELOPER,<br> UI AND UX DESIGNER</div>
             </v-col>
           </v-row>
         </v-col>
@@ -33,11 +24,11 @@
 
 <script>
 import Navbar from "../components/Navbar";
-import Profile from "../components/Profile";
 import Spinner from "../components/Spinner";
+import Profile from "../components/Profile";
 
 export default {
-  components: { Navbar, Profile, Spinner },
+  components: { Navbar, Spinner, Profile },
   data() {
     return {
       show: false,
@@ -52,10 +43,15 @@ export default {
 };
 </script>
 <style scoped>
-.slide-left {
-  margin-top: 10%;
+.display-font-7{
+    font-size: 7rem !important;
+    font-weight: 300;
+    font-family: "Roboto", sans-serif !important;
 }
-.slide-right {
-  margin-top: 25%;
+.uppercase {
+  text-transform: uppercase;
+}
+.top {
+  margin-top: 8%;
 }
 </style>
