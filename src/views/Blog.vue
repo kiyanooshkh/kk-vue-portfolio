48bdcb6e6b89b948970bd05c86e941b85171809c
Kiyanoosh Khosravi <Kiyanoosh.Khosravi@certegy.com.au>
2019-10-01 14:16:38 +0930

    Update Blog.vue

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -5,7 +5,19 @@
       <v-row>
         <Sidebar pageTitle="Blog" />
         <v-col cols="12" md="12" lg="9" class="pt-0 pb-0">
-
+          <template >
+            <v-row>
+              <v-col
+                no-gutters
+                class="pa-0"
+                cols="12"
+                v-for="content in contents"
+                :key="content.id"
+              >
+                <span v-html="content.content.rendered"></span>
+              </v-col>
+            </v-row>
+          </template>
         </v-col>
       </v-row>
     </div>
@@ -15,16 +27,34 @@
 <script>
 import Sidebar from "../components/Sidebar";
 import Spinner from "../components/Spinner";
+import axios from "axios";
 
 export default {
   components: { Sidebar, Spinner },
   data() {
     return {
       show: false,
-      noRadius: true
+      noRadius: true,
+      url: "http://kk-blog.ptshosting.com/wp-json/wp/v2/posts/",
+      contents: [],
+      error: false
     };
   },
+  methods: {
+    getContents() {
+      axios
+        .get(this.url)
+        .then(({ data }) => {
+          this.contents = data;
+        })
+        .catch(function(errorMessage) {
+          console.log(errorMessage);
+          this.error = true;
+        });
+    }
+  },
   created() {
+    this.getContents();
     setInterval(() => {
       this.show = true;
     }, 2000);
@@ -32,5 +62,4 @@ export default {
 };
 </script>
 <style scoped>
-
 </style>
